368b06429f33b22cb21dbf326dc93f73a6ac9dcc
TruongNBN <mylove.lanhuong@gmail.com>
2023-04-23 12:02:07 +0700

    init app

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BlockchainApp {
+    class Program {
+        static void Main(string[] args) {
+            Console.OutputEncoding = Encoding.UTF8;
+            Blockchain blockchain = new Blockchain();
+            blockchain.AddBlock(new Block(DateTime.Now, "Block 1", ""));
+            blockchain.AddBlock(new Block(DateTime.Now, "Block 2", ""));
+            blockchain.AddBlock(new Block(DateTime.Now, "Block 3", ""));
+
+            Console.WriteLine($"Is blockchain valid? {blockchain.IsChainValid()}");
+            Console.WriteLine($"Latest block: {blockchain.GetLatestBlock().Data}");
+        }
+    }
+}
---
+++ src/Block.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BlockchainApp {
+    class Block {
+        public int Index { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Data { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+
+        public Block(DateTime timestamp, string data, string previousHash) {
+            Index = 0;
+            Timestamp = timestamp;
+            Data = data;
+            PreviousHash = previousHash;
+            Hash = CalculateHash();
+        }
+
+        public string CalculateHash() {
+            SHA256 sha256 = SHA256.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes($"{Timestamp}-{Data}-{PreviousHash}");
+            byte[] outputBytes = sha256.ComputeHash(inputBytes);
+            return Convert.ToBase64String(outputBytes);
+        }
+    }
+}
