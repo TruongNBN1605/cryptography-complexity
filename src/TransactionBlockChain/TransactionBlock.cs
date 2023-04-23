94f5258a2955aafa64677a24a608aafdac5d7c28
TruongNBN <mylove.lanhuong@gmail.com>
2023-04-23 15:30:54 +0700

    fix crash app

--- src/TransactionBlockChain/TransactionBlock.cs
+++ src/TransactionBlockChain/TransactionBlock.cs
@@ -2,11 +2,11 @@ using System;
 using VirtualMoney;
 using BlockchainBase;
 
-namespace TransactionBlockChain {
-    class TransactionBlock: Block {
-        public Transaction Data_ { get; set; }
+// namespace TransactionBlockChain {
+//     class TransactionBlock: Block {
+//         public Transaction Data_ { get; set; }
 
-        public TransactionBlock(DateTime timestamp, Transaction data_, string previousHash) {
-        }
-    }
-}
+//         public TransactionBlock(DateTime timestamp, Transaction data_, string previousHash) {
+//         }
+//     }
+// }
