944bc336c497c2f028a71e905ed3f48b62eebab7
TruongNBN <mylove.lanhuong@gmail.com>
2023-04-23 12:14:51 +0700

    add introduce group + add gitignore

--- Program.cs
+++ Program.cs
@@ -5,8 +5,27 @@ using System.Text;
 
 namespace BlockchainApp {
     class Program {
+        public static void introduceGroup () {
+            Console.WriteLine("Môn học \t: Cryptography & Complexity");
+            Console.WriteLine("Giảng viên \t: Nguyễn ĐÌnh Hân");
+            Console.WriteLine("Nhóm \t: Nhóm 9");
+            Console.WriteLine("Thành viên \t:");
+            Console.WriteLine("\t 20185418 - Nguyễn Bùi Nam Trường");
+            Console.WriteLine("\t 20185400 - Nguyễn Trường Sơn");
+            Console.WriteLine("\t 20185420 - Nguyễn Minh Tuấn");
+            Console.WriteLine("\t ######## - Nguyễn Tiến Vỹ");
+            Console.WriteLine("\t 20185390 - Nguyễn Phú Nhật");
+            Console.WriteLine("_______________________________________");
+        }
+
         static void Main(string[] args) {
+            // Config app
             Console.OutputEncoding = Encoding.UTF8;
+
+            // introduce group
+            introduceGroup();
+
+            // Action: Demo Blockchain App
             Blockchain blockchain = new Blockchain();
             blockchain.AddBlock(new Block(DateTime.Now, "Block 1", ""));
             blockchain.AddBlock(new Block(DateTime.Now, "Block 2", ""));
